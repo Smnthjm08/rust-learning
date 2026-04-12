7cf8946b252631af1295114b567653c5d82cd5ff
Smnthjm08 <sumanthjmm.04@gmail.com>
2026-04-12 19:35:18 +0530

    week 38-anchor calculator program init

---
+++ calculator-program/programs/calculator-program/src/lib.rs
@@ -0,0 +1,18 @@
+use anchor_lang::prelude::*;
+
+declare_id!("AZuyM9B7SGfC6jSJiCdutUTDBtNxFxnJk6rKknJCX4zD");
+
+#[program]
+pub mod calculator_program {
+    use super::*;
+
+    // pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    //     msg!("Greetings from: {:?}", ctx.program_id);
+    //     Ok(())
+    // }
+}
+
+// #[derive(Accounts)]
+// pub struct Initialize {
+
+// }
